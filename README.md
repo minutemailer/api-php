abdf8952882c842f9fcfa9d20ce938e220d34829
Tobias Bleckert <hola@tobiasbleckert.se>
2015-08-30 02:30:41 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Minutemailer API
+================
---
+++ src/Minutemailer/Minutemailer.php
@@ -0,0 +1,61 @@
+<?php namespace Minutemailer;
+
+use \GuzzleHttp\Client;
+
+class Minutemailer
+{
+
+	private $baseUrl;
+	private $urlAccessToken;
+	private $grantType;
+	private $currentEndpoint;
+	private $version;
+	private $clientSecret;
+	private $clientId;
+	private $client;
+
+	public function __construct($clientId, $clientSecret)
+	{
+		$this->baseUrl        = 'https://api.minutemailer.com';
+		$this->urlAccessToken = '/oauth/access_token';
+		$this->version        = 'v1';
+		$this->grantType      = 'password';
+		$this->clientId       = $clientId;
+		$this->clientSecret   = $clientSecret;
+		$this->client         = new Client(['base_uri' => $this->baseUrl]);
+
+		$this->getAccessToken();
+	}
+
+	public function getAccessToken()
+	{
+		$response = $this->client->post($this->urlAccessToken, ['form_params' => [
+			'grant_type'    => $this->grantType,
+			'client_id'     => $this->clientId,
+			'client_secret' => $this->clientSecret
+		]]);
+
+		if ($response->getBody()) {
+			$json = json_decode($response->getBody());
+			
+			$this->accessToken = $json->access_token;
+		}
+	}
+
+	public function subscribe($token)
+	{
+		$this->currentEndpoint = '/subscribe/' . $token;
+		return $this;
+	}
+
+	public function post($data)
+	{
+		$data['access_token'] = $this->accessToken;
+
+		$response = $this->client->post('/' . $this->version . $this->currentEndpoint, ['form_params' => $data]);
+		$response = json_decode($response->getBody());
+
+		return $response;
+	}
+
+}
